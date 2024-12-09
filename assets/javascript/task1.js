b550db712bc2054bb86b1b0d8a1b12693080e702
20312 <jason.ziegler@vtsd.com>
2024-12-09 09:24:15 -0500

    Test Progress

--- assets/javascript/task1.js
+++ assets/javascript/task1.js
@@ -1,1 +1,11 @@
 /* Put code for the task below */
+
+
+        function appear(){
+    document.getElementById('buttonTask1').style.display = "show";
+
+}
+
+    function appear(){
+    document.getElementById('imageTask1').style.display = "none";
+}
--- assets/javascript/task2.js
+++ assets/javascript/task2.js
@@ -1,1 +1,15 @@
 /* Put code for the task below */
+
+document.getElementById("table")
+counter = 0;
+function toggle(){
+
+    if (counter % 2 === 0) {
+        table.style.backgroundColor = "red"
+        {else}
+        table.style.backgroundColor = "blue"
+    }
+    counter++;
+
+
+}
